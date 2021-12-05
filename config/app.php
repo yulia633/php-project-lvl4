7132a3c96127fa93a34ed1d29874123ce01341f7
yulia633 <ignat633yulia@yandex.ru>
2021-12-05 21:53:31 +0300

    add ide_helper

--- config/app.php
+++ config/app.php
@@ -166,7 +166,7 @@ return [
          * Package Service Providers...
          */
         Rollbar\Laravel\RollbarServiceProvider::class,
-
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         /*
          * Application Service Providers...
          */
