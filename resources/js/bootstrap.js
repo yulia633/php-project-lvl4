1fcd52385887cf65c53b7ffd8fad48410718c285
yulia633 <ignat633yulia@yandex.ru>
2021-12-05 11:17:41 +0300

    add dependencies for css script

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,16 @@
 window._ = require('lodash');
 
-try {
-    require('bootstrap');
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+ try {
+  window.Popper = require('popper.js').default;
+  window.$ = window.jQuery = require('jquery');
+
+  require('bootstrap');
 } catch (e) {}
 
 /**
