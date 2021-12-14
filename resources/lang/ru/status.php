99b68dbfc7d039a7b67a1bdcd19ff4170ea0b42d
yulia633 <ignat633yulia@yandex.ru>
2021-12-14 23:43:10 +0300

    add ru locale

---
+++ resources/lang/ru/status.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'Status' => 'Статус',
+    'Create status' => 'Создать статус',
+    'Status name' => 'Имя',
+    'Create' => 'Создать',
+    'Statuses' => 'Статусы',
+    'Status edit' => 'Изменение статуса',
+    'Edit' => 'Изменить',
+    'Delete' => 'Удалить',
+    'Update' => 'Обновить',
+    'Date of creation' => 'Дата создания',
+    'Actions' => 'Действия',
+    'Apply' => 'Применить',
+    'Status has been added successfully' => 'Статус успешно создан',
+    'Status has been updated successfully' => 'Статус успешно изменён',
+    'Status has been deleted successfully' => 'Статус успешно удалён',
+    'Failed to delete status' => 'Не удалось удалить статус',
+    'Description' => 'Описание',
+    'Are you sure?' => 'Вы уверены?',
+];
