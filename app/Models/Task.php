e157c37e328b4825bf0746a5767d01f24fb8dc6e
yulia633 <ignat633yulia@yandex.ru>
2021-12-21 18:57:51 +0300

    update models: task, task status, user

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'status_id', 'created_by_id', 'assigned_to_id'];
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(TaskStatus::class);
+    }
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by_id');
+    }
+
+    public function executor(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'assigned_to_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -43,4 +44,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class, 'created_by_id');
+    }
 }
