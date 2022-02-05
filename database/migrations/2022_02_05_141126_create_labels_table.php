af29e5bff5609d3663b0344ee80dcd4006779213
yulia633 <ignat633yulia@yandex.ru>
2022-02-05 17:15:09 +0300

    add migrations labels

---
+++ database/migrations/2022_02_05_141126_create_labels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLabelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('labels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->timestamp('updated_at')->default(now());
+            $table->timestamp('created_at')->default(now());
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('labels');
+    }
+}
