e57ec22f6e766c7beb3163e916e406aa20ef9ddf
yulia633 <ignat633yulia@yandex.ru>
2022-02-05 17:47:58 +0300

    add model label and relations

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Task extends Model
 {
@@ -25,5 +26,10 @@ class Task extends Model
     public function executor(): BelongsTo
     {
         return $this->belongsTo(User::class, 'assigned_to_id');
+    }
+
+    public function labels(): BelongsToMany
+    {
+        return $this->belongsToMany(Label::class);
     }
 }
