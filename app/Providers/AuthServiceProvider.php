2b8e669dcbe0296a94dc79a6148e775a1fef0a6b
yulia633 <ignat633yulia@yandex.ru>
2022-02-06 13:07:57 +0300

    add polices and registration for label

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Label;
 use App\Models\TaskStatus;
 use App\Models\Task;
+use App\Policies\LabelPolicy;
 use App\Policies\TaskStatusPolicy;
 use App\Policies\TaskPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -18,6 +20,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         TaskStatus::class => TaskStatusPolicy::class,
         Task::class => TaskPolicy::class,
+        Label::class => LabelPolicy::class,
     ];
 
     /**
