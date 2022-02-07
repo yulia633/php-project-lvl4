97540a5c86b1555295989d4801afb1f270312a0a
yulia633 <ignat633yulia@yandex.ru>
2022-02-07 21:34:01 +0300

    add migration for relations task and label

---
+++ database/migrations/2022_02_06_103957_create_label_task_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLabelTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('label_task', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_id')->constrained('tasks');
+            $table->foreignId('label_id')->constrained('labels');
+            $table->timestamp('updated_at')->default(now());
+            $table->timestamp('created_at')->default(now());
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('label_task');
+    }
+}
