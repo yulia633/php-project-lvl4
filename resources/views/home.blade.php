f0710ff7e3043c01fa2a7f8c76481830f46d9dd4
yulia633 <ignat633yulia@yandex.ru>
2022-02-15 21:33:13 +0300

    fix auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+<main class="container py-4">
+    <div class="p-5 mb-4 bg-light border rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Привет от Хекслета!</h1>
+            <p class="col-md-8 fs-4">Практические курсы по программированию</p>
+            <button href="https://ru.hexlet.io/" class="btn btn-primary btn-lg" type="button">Узнать больше</button>
         </div>
     </div>
-</div>
+</main>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,13 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__ . '/auth.php';
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
 Route::resources([
     'task_statuses' => TaskStatusController::class,
