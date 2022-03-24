d01bcac7e444c710d83e13d3390903f2b1d92608
yulia633 <ignat633yulia@yandex.ru>
2022-03-24 12:01:24 +0300

    check #1: add lang ru for home views

---
+++ resources/lang/ru/home.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Home Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Hello from Hexlet!' => 'Привет от Хекслета!',
+    'Practical programming courses!' => 'Практические курсы по программированию',
+    'Learn more' => 'Узнать больше'
+];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
 <main class="container py-4">
     <div class="p-5 mb-4 bg-light border rounded-3">
         <div class="container-fluid py-5">
-            <h1 class="display-5 fw-bold">Привет от Хекслета!</h1>
-            <p class="col-md-8 fs-4">Практические курсы по программированию</p>
-            <button href="https://ru.hexlet.io/" class="btn btn-primary btn-lg" type="button">Узнать больше</button>
+            <h1 class="display-5 fw-bold">{{ __('home.Hello from Hexlet!') }}</h1>
+            <p class="col-md-8 fs-4">{{ __('home.Practical programming courses!') }}</p>
+            <button href="https://ru.hexlet.io/" class="btn btn-primary btn-lg" type="button">{{ __('home.Learn more') }}</button>
         </div>
     </div>
 </main>
